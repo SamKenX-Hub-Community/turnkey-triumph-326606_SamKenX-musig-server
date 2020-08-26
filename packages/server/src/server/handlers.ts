ae035f3ead95ac53794d8ba5be4d1d273fb65296
Brian Faust <faustbrian@users.noreply.github.com>
2020-08-26 14:21:23 +0300

    fix: handle request errors (#89)

--- packages/server/src/server/handlers.ts
+++ packages/server/src/server/handlers.ts
@@ -5,45 +5,71 @@ import { TransactionStatus } from "./enums";
 import { getBaseTransactionId, verifySignatures } from "./utils";
 
 export const getTransactions = (request, h) => {
-    if (request.query.publicKey) {
-        const storeTransactions = memory.getTransactionsByPublicKey(request.query.publicKey);
+    try {
+        if (request.query.publicKey) {
+            const storeTransactions = memory.getTransactionsByPublicKey(request.query.publicKey);
 
-        if (request.query.state === TransactionStatus.Pending) {
-            return storeTransactions.filter(t => t.data.signatures.length < t.multisigAsset.min);
-        } else if (request.query.state === TransactionStatus.Ready) {
-            return storeTransactions.filter(t => t.data.signatures.length >= t.multisigAsset.min);
+            if (request.query.state === TransactionStatus.Pending) {
+                return storeTransactions.filter((t) => t.data.signatures.length < t.multisigAsset.min);
+            }
+
+            if (request.query.state === TransactionStatus.Ready) {
+                return storeTransactions.filter((t) => t.data.signatures.length >= t.multisigAsset.min);
+            }
+
+            return storeTransactions;
         }
-        return storeTransactions;
-    }
 
-    return memory.getAllTransactions(); // keep or throw error ? (why would we need to get all tx ?)
+        return memory.getAllTransactions(); // keep or throw error ? (why would we need to get all tx ?)
+    } catch (error) {
+        return Boom.badImplementation(error.message);
+    }
 };
 
 export const getTransaction = (request, h) => {
-    return memory.getTransactionById(request.params.id);
+    try {
+        const transaction = memory.getTransactionById(request.params.id);
+
+        if (transaction === undefined) {
+            return Boom.notFound(`Failed to find transaction [${request.params.id}]`);
+        }
+
+        return transaction;
+    } catch (error) {
+        return Boom.badImplementation(error.message);
+    }
 };
 
 export const postTransaction = (request, h) => {
-    const transaction: IStoreTransaction = request.payload;
+    try {
+        const transaction: IStoreTransaction = request.payload;
 
-    if (transaction.data.signatures && transaction.data.signatures.length) {
-        if (!verifySignatures(transaction.data, transaction.multisigAsset)) {
-            return Boom.badData("Transaction signatures are not valid");
+        if (transaction.data.signatures && transaction.data.signatures.length) {
+            if (!verifySignatures(transaction.data, transaction.multisigAsset)) {
+                return Boom.badData("Transaction signatures are not valid");
+            }
         }
+
+        const baseTransactionId = getBaseTransactionId(transaction.data);
+        const storeTransaction = memory.getTransactionById(baseTransactionId);
+        if (storeTransaction) {
+            memory.updateTransaction(transaction.data);
+
+            return { id: baseTransactionId };
+        }
+
+        return { id: memory.saveTransaction(transaction) };
+    } catch (error) {
+        return Boom.badImplementation(error.message);
     }
-
-    const baseTransactionId = getBaseTransactionId(transaction.data);
-    const storeTransaction = memory.getTransactionById(baseTransactionId);
-    if (storeTransaction) {
-        memory.updateTransaction(transaction.data);
-
-        return { id: baseTransactionId };
-    }
-
-    return { id: memory.saveTransaction(transaction) };
 };
 
 export const deleteTransactions = (request, h) => {
-    memory.deleteAllTransactions();
-    return true;
+    try {
+        memory.deleteAllTransactions();
+
+        return h.response().code(204);
+    } catch (error) {
+        return Boom.badImplementation(error.message);
+    }
 };
